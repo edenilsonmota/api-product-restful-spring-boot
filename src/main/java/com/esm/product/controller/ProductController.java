6eab5359d592e341791beac84f032e99e6612122
Edenilson Mota <edenilsonmota.job@gmail.com>
2024-10-17 20:05:11 -0300

    Feat: Create method POST products

---
+++ src/main/java/com/esm/product/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.esm.product.controller;
+
+import com.esm.product.dto.ProductRecordDto;
+import com.esm.product.model.ProductModel;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.esm.product.repository.ProductRepository;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+}
